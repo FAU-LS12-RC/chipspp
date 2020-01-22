37ab17aea8d7d306fac737075b22be8b85ae9cee
Patrick Plagwitz <patrick.plagwitz@fau.de>
2020-01-22 14:17:10 +0100

    Fix LLVM build script.

--- install-llvm.sh
+++ install-llvm.sh
@@ -1,12 +1,13 @@
 set -e
 installDir="$1"
+srcDir=llvm-src
 
 if [ -e "$installDir"/bin/llvm-config ]; then
   exit
 fi
 
-git clone https://github.com/llvm/llvm-project.git llvm-src
-cd llvm-project
+git clone https://github.com/llvm/llvm-project.git "$srcDir"
+cd "$srcDir"
 git checkout llvmorg-6.0.0
 cd llvm
 mkdir build
