7eb428df5e5468080b6ac59ccae9082c1f103176
Patrick Plagwitz <patrick.plagwitz@fau.de>
2020-01-22 10:38:33 +0100

    Add LLVM build script.

---
+++ install-llvm.sh
@@ -0,0 +1,17 @@
+set -e
+installDir="$1"
+
+if [ -e "$installDir"/bin/llvm-config ]; then
+  exit
+fi
+
+git clone https://github.com/llvm/llvm-project.git llvm-src
+cd llvm-project
+git checkout llvmorg-6.0.1
+cd llvm
+mkdir build
+cd build
+cmake ..
+cmake --build .
+cmake -DCMAKE_INSTALL_PREFIX="$installDir" -P cmake_install.cmake
+#rm -r llvm-src
